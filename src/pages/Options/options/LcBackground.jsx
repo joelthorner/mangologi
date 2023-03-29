058cec34eb4d8edfdaa2967c8ba260e0e9af3551
Joelthorner <joeltorner@gmail.com>
2023-03-29 12:40:32 +0200

    :ambulance:

--- src/pages/Options/options/LcBackground.jsx
+++ src/pages/Options/options/LcBackground.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import Form from 'react-bootstrap/Form';
 import Content from '../components/Content';
-
 import ExtraChromeSyncData from '../../../data/extraChromeSyncData.json';
 import OptionContentHeader from './common/OptionContentHeader';
 import ActiveSwitch from './common/ActiveSwitch';
+import Collections from './lcBackground/Collections';
 
 const KEY = 'lcBackground';
 
@@ -19,6 +19,17 @@ const LcBackground = () => {
       <OptionContentHeader keyData={KEY} extraData={extraData} />
       <Content>
         <ActiveSwitch keyData={KEY} data={data} />
+
+        <div className="lc-background-content">
+          <div className="col">
+            <Form.Control
+              type="search"
+              placeholder={chrome.i18n.getMessage(`searchPlaceholder`)}
+            />
+            <Collections />
+          </div>
+          <div className="col-content">cont</div>
+        </div>
       </Content>
     </>
   );
