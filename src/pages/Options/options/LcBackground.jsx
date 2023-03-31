3433cf9be0abe14535231eff8d4a6ed92c55724b
Joelthorner <joeltorner@gmail.com>
2023-03-31 09:10:49 +0200

    :pear:

--- src/pages/Options/options/LcBackground.jsx
+++ src/pages/Options/options/LcBackground.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import Content from '../components/Content';
 import ExtraChromeSyncData from '../../../data/extraChromeSyncData.json';
+import { fetchRandomImages } from '../../../slices/unsplashSlice';
 import OptionContentHeader from './common/OptionContentHeader';
 import ActiveSwitch from './common/ActiveSwitch';
 import Collections from './lcBackground/Collections';
@@ -11,9 +12,34 @@ import SelectedBg from './lcBackground/SelectedBg';
 const KEY = 'lcBackground';
 
 const LcBackground = () => {
+  const dispatch = useDispatch();
   const chromeSync = useSelector((state) => state.chromeSync);
   const extraData = ExtraChromeSyncData[KEY];
   const data = chromeSync.storage[KEY];
+  const { randomImages, loading } = useSelector((state) => state.unsplash);
+
+  useEffect(() => {
+    dispatch(fetchRandomImages());
+  }, [dispatch]);
+
+  // const dispatch = useDispatch();
+  // const { loading, error, randomImages } = useSelector(randomImagesSelector);
+
+  // useEffect(() => {
+  //   dispatch(fetchRandomImages());
+  // }, [dispatch]);
+
+  // // render the items
+  // const renderItems = () => {
+  //   // loading state
+  //   if (loading) return <strong>Loading please wait...</strong>;
+
+  //   // error state
+  //   if (error) return <strong>Items not available at this time</strong>;
+
+  //   // regular data workflow
+  //   return randomImages.map((i) => console.log(i));
+  // };
 
   return (
     <>
@@ -31,7 +57,16 @@ const LcBackground = () => {
             <Collections />
           </div>
           <div className="col-content">
-            <div className="grid-background-items"></div>
+            <div className="grid-background-items">
+              {!loading ? console.log(randomImages) : <div>Loading...</div>}
+              {/* <button
+                onClick={() => {
+                  // dispatch(getRandomUnsplash());
+                }}
+              >
+                ljkahsdkjlashdhas
+              </button> */}
+            </div>
           </div>
         </div>
       </Content>
