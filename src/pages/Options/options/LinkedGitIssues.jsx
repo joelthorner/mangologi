9cce233bd0a94bd63df7ca4f5c249a87b0951a5e
Joelthorner <joeltorner@gmail.com>
2023-04-04 12:59:19 +0200

    :bee:

--- src/pages/Options/options/LinkedGitIssues.jsx
+++ src/pages/Options/options/LinkedGitIssues.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import OptionContentHeader from './common/OptionContentHeader';
+import Content from '../components/Content';
+import ExtraChromeSyncData from '../../../data/extraChromeSyncData.json';
+import ActiveSwitch from './common/ActiveSwitch';
+
+const KEY = 'linkedGitIssues';
 
 const LinkedGitIssues = () => {
-  return <div>LinkedGitIssues</div>;
+  const extraData = ExtraChromeSyncData.linkedGitIssues;
+  const chromeSync = useSelector((state) => state.chromeSync);
+  const data = chromeSync.storage.linkedGitIssues;
+
+  return (
+    <>
+      <OptionContentHeader keyData={KEY} extraData={extraData} />
+      <Content>
+        <div className="default-option-content linked-git-issues">
+          <ActiveSwitch keyData={KEY} data={data} />
+        </div>
+      </Content>
+    </>
+  );
 };
 
 export default LinkedGitIssues;
--- src/pages/Options/options/ScrollToDump.jsx
+++ src/pages/Options/options/ScrollToDump.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import OptionContentHeader from './common/OptionContentHeader';
+import Content from '../components/Content';
+import ExtraChromeSyncData from '../../../data/extraChromeSyncData.json';
+import ActiveSwitch from './common/ActiveSwitch';
+
+const KEY = 'scrollToDump';
 
 const ScrollToDump = () => {
-  return <div>ScrollToDump</div>;
+  const extraData = ExtraChromeSyncData.scrollToDump;
+  const chromeSync = useSelector((state) => state.chromeSync);
+  const data = chromeSync.storage.scrollToDump;
+
+  return (
+    <>
+      <OptionContentHeader keyData={KEY} extraData={extraData} />
+      <Content>
+        <div className="default-option-content scroll-to-dump">
+          <ActiveSwitch keyData={KEY} data={data} />
+        </div>
+      </Content>
+    </>
+  );
 };
 
 export default ScrollToDump;
